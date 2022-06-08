de8cff614af2ece46834617f8e4bfa965eff8f01
Alexander <alexanderlukiyanovspb@gmail.com>
2022-06-08 22:02:53 +0300

    Домашнее задание 9

---
+++ Lesson_9/spring-web/src/main/java/com/geekbrains/spring/web/services/ProductService.java
@@ -0,0 +1,77 @@
+package com.geekbrains.spring.web.services;
+
+import com.geekbrains.spring.web.data.Product;
+import com.geekbrains.spring.web.exceptions.AppError;
+import com.geekbrains.spring.web.repositories.ProductRepository;
+import com.geekbrains.spring.web.repositories.spetification.ProductSpecification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    private ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Page<Product> findAllProduct(Integer p, Integer maxScore, Integer minScore, String partName){
+        Specification<Product> specProduct = Specification.where(null);
+        if(minScore != null){
+            specProduct = specProduct.and(ProductSpecification.scoreGreaterOrElseThan(minScore));
+        }
+        if(maxScore != null){
+            specProduct = specProduct.and(ProductSpecification.lessThanOrEqualTo(maxScore));
+        }
+        if(partName != null){
+            specProduct = specProduct.and(ProductSpecification.nameLike(partName));
+        }
+        return productRepository.findAll(specProduct, PageRequest.of(p -1, 5));
+    }
+
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public List<Product> findByScore(Integer min, Integer max) {
+        return productRepository.findAllByAmountBetween(min, max);
+    }
+
+    @Transactional
+    public void changeAmount(Long productId, Integer delta) {
+        Product product = productRepository.findById(productId).orElseThrow();
+        product.setAmount(product.getAmount() + delta);
+        //studentRepository.save(student);
+    }
+
+    //public List<Product> findByAmountBetween(Integer min, Integer max) {
+    //    return findByAmountBetween(min, max);
+    //}
+
+    public ResponseEntity<?> addProduct(Product product) {
+        if(productRepository.existsProductByName(product.getName())){
+            return new ResponseEntity<>(new AppError(HttpStatus.CONFLICT.value(), "Product is already"), HttpStatus.CONFLICT);
+        } else {
+            productRepository.save(product);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+    }
+
+    public List<Product> findRangeProduct(Integer min, Integer max) {
+        return productRepository.findRangeProduct(min, max);
+
+    }
+}
